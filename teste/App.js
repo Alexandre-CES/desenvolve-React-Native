a7acd17f19a37203744a3949490895a28bc05d8e
Alexandre <alexandreces666@gmail.com>
2025-04-18 14:22:17 -0300

    button

--- teste/App.js
+++ teste/App.js
@@ -1,22 +1,31 @@
 import { React, Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text,  Button } from 'react-native';
 
 class App extends Component{
-    render(){
-        let nome = 'Alexandre';
-        let img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ18Gt9viR2yfaYTVjyutOeMAGk6sFTje4g2A&s';
+    constructor(props){
+        super(props);
+
+        this.state = {
+            nome:'Carlos'
+        };
+        this.entrar = this.entrar.bind(this);
+    };
     
-        return (
-            <View style={{height:'100%', backgroundColor:'white'}}>
-                <Text>Meu primeiro App</Text>
-                <Text style={{color:'blue', fontSize:30, margin:70}}>Vamos Programar</Text>
-                <Text style={{color:'blue', fontSize:30, left:150}}>{nome}</Text>
-                <Image 
-                    source={{uri:img}}
-                    style={{width:300, height:300, left:50}}
-                />
+    entrar(){
+        this.setState({
+            nome:'Robson'
+        });
+    }
+
+    render(){
+        return(
+            <View style={{marginTop:30, }}>
+                <Button title='Entrar' onPress={this.entrar}/>
+                <Text style={{fontSize:30, color:'blue',textAlign:'center'}}>
+                    {this.state.nome}
+                </Text>
             </View>
-        )
+        );
     }
 }
 
