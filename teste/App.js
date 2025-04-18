0385f6c4d240a67ffa08af1485942e1e7fb0fc29
Alexandre <alexandreces666@gmail.com>
2025-04-18 17:23:26 -0300

    using App as a function

--- teste/App.js
+++ teste/App.js
@@ -1,32 +1,14 @@
-import { React, Component } from 'react';
-import { View, Text,  Button } from 'react-native';
+import { React } from 'react';
+import { View, Text } from 'react-native';
 
-class App extends Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            nome:'Carlos'
-        };
-        this.entrar = this.entrar.bind(this);
-    };
-    
-    entrar(){
-        this.setState({
-            nome:'Robson'
-        });
-    }
-
-    render(){
-        return(
-            <View style={{marginTop:30, }}>
-                <Button title='Entrar' onPress={this.entrar}/>
-                <Text style={{fontSize:30, color:'blue',textAlign:'center'}}>
-                    {this.state.nome}
-                </Text>
-            </View>
-        );
-    }
+const App = ()=>{
+    return (
+        <View>
+            <Text>
+                Olá programadores, tudo bem?
+            </Text>
+        </View>
+    )
 }
 
 export default App;
