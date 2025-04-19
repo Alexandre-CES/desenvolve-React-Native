5155e06c304858bbfa907333026f4f360c52d3df
Alexandre <alexandreces666@gmail.com>
2025-04-19 14:10:46 -0300

    align items center

--- teste/App.js
+++ teste/App.js
@@ -3,13 +3,23 @@ import { View, Text, Image } from 'react-native';
 
 const App = ()=>{
     return (
-        <View>
-            <Text style={{fontSize:25,color:'blue', textAlign:'center'}}>
+        <View style={{alignItems:'center'}}>
+            <Text 
+                style={{
+                    fontSize:25,
+                    color:'blue', 
+                    textAlign:'center'
+                }}
+            >
                 Olá programadores, tudo bem?
             </Text>
             <Image 
                 source={require('./src/reactNative.png')}
-                style={{width:100, height:100}}
+                style={{width:300, height:300}}
+            />
+            <Image 
+                source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/250px-React-icon.svg.png'}}
+                style={{width:400, height:400, marginTop:100}}
             />
         </View>
     )
